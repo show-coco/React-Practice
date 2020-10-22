0c883182f0672c9f6c0cf210ecb48d475d5b7c8b
show <show75@ezweb.ne.jp>
2020-10-22 21:44:36 +0900

    Try to use useLayoutEffect

---
+++ react-component-design/src/components/hooks/useMousePosition.tsx
@@ -0,0 +1,19 @@
+import { useState, useLayoutEffect } from "react";
+import { WSAEINVALIDPROCTABLE } from "constants";
+
+export const useMousePosition = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const setPosition = ({ x, y }: { x: number; y: number }) => {
+    setX(x);
+    setY(y);
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener("mousemove", setPosition);
+    return () => window.removeEventListener("mousemove", setPosition);
+  }, [])
+
+  return [x, y];
+};
